d352874219a9645c799926c7b88192210ebb3596
Pablo Torrez <pablotorrez90@outlook.com>
2024-08-26 15:37:08 -0300

    config

--- src/util.js
+++
@@ -1,9 +0,0 @@
-// Funciones auxiliares para operaciones comunes
-export function validarUsername(username) {
-    return username && username.length > 2;
-}
-
-export function validarEmail(email) {
-    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return re.test(email);
-}
