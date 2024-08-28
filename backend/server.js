523cbe1f1803cc01b9bc73c4cfa76734e90eaa47
Pablo Torrez <pablotorrez90@outlook.com>
2024-08-28 18:00:41 -0300

    agregando formularios y configurando el backend

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 
 /**
+ * 
+ * Lado del servidor y bd (backend)
+ * 
  * CORS es importante cuando tu frontend y backend están en diferentes dominios o puertos. Sin él, las solicitudes entre dominios podrían ser bloqueadas por el navegador debido a las políticas de seguridad.
  */
 
@@ -12,6 +15,7 @@ const app = express() // inicializo una instancia para ejecutar el servidor
 app.use(express.json()) // middleware para parsear el body (sin este middleware el body seria undefined)
 app.use(cors()) // para habilitar CORS y permite que otros origenes consuman el servidor
 
+// conexion a la db
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -19,20 +23,30 @@ const db = mysql.createConnection({
     database: 'db_gestion_de_tareas'
 })
 
+// solicitud de post con el endpoint `/register` y manejo de errores
 app.post('/register', (req, res) => {
-    const { nombreApellido, email, password } = req.body;
+    const { nombreApellido, email, password } = req.body; // obtengo los datos q ingresa el usuario
+    // manejo de errores en el endpoint de registro (`/register`)
+    // si el usuario no envia todos los datos, lanza errores de estados
     if (!nombreApellido || !email || !password) {
-        return res.status(400).json({ error: "Todos los campos son obligatorios" });
+        if (!nombreApellido) return res.status(400).json({ error: "El nombre es obligatorio" });
+
+        if (!email) return res.status(400).json({ error: "El Email es obligatorio" });
+
+        if (!password) return res.status(400).json({ error: "La contraseña es obligatoria" });
+
+        if (password.length < 6) return res.status(400).json({ error: "La contraseña debe tener al menos 6 caracteres" });
     }
 
     const sql = "INSERT INTO USUARIOS (NOMBRE_APELLIDO, EMAIL, PASSWORD) VALUES (?, ?, ?)";
+    // manejo del resultado de inserción en la bd
     db.query(sql, [nombreApellido, email, password], (err, data) => {
-        if (err) return res.status(500).json(err);
-        return res.status(200).json({ message: "Registrado Exitosamente" });
+        if (err) return res.status(500).json({error: "Error en el servidor", details: err}); // manejo de error de servidor
+        return res.status(200).json({ message: "¡Registrado Exitosamente!" }); // estado OK 200 y el mensaje
     });
 });
 
 // probar el server
 app.listen(8081, () => {
-    console.log("Listening on port 8081...");
+    console.log("Server in port 8081...");
 });
--- src/registro.js
+++ src/registro.js
@@ -1,15 +1,44 @@
+
+/**
+ * Lado del cliente (Frontend)
+*/
+
 // import axios from 'axios';
 
+const infoConnection = document.getElementById("connection-status");
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById("login-form");
+    // console.log(form === null);
     form.addEventListener("submit", (event) => {
         event.preventDefault();
         const nombreApellido = document.getElementById("name-and-lastname").value;
+        // console.log(nombreApellido)
         const email = document.getElementById("email").value;
+        // console.log(email)
         const password = document.getElementById("password").value;
+        // console.log(password)
 
+        // hago una solicitud POST al servidor, con la URL del endpoint del servidor y los datos del usuario que se envían en la solicitud para registrar al usuario
         axios.post('http://localhost:8081/register', { nombreApellido, email, password })
-            .then(res => console.log(res.data))
-            .catch(err => console.log(err.response ? err.response.data : err.message));
+            .then(res => {
+                console.log(res); // { data: {...}, status: 200, statusText: 'OK', headers: {...}, config: {...} }
+                console.log(res.data); // {message: '¡Registrado Exitosamente!'}
+                if (res.data.message === "¡Registrado Exitosamente!") {
+                    console.log(res); // { data: {...}, status: 200, statusText: 'OK', headers: {...}, config: {...} }
+                    console.log(res.data); // { message: '¡Registrado Exitosamente!' }
+                    console.log(res.data.message); // ¡Registrado Exitosamente!
+                    infoConnection.innerText = res.data.message; // Almacena en el elemento <span> el mensaje
+                }
+            })
+            .catch(err => {
+                console.log("Hubo un error al registrar el usuario: ", err.response ? err.response.data : err.message)
+                if(infoConnection.innerText.indexOf("Hubo un error al registrar el usuario") === -1){ // le pongo '-1' para preguntar si no hay 'Hubo un error al registrar el usuario'
+                    infoConnection.innerText = "Hubo un error al registrar el usuario"; // lo agrega si no hay
+                } else if (infoConnection.innerText.indexOf("Registro exitoso") === -1){ // sino, si el contenido no tiene "Registro exitoso" que lo agregue
+                    infoConnection.innerText = "Registro exitoso"; // lo agrega si no hay
+                } else {
+                    infoConnection.innerText = err.response ? err.response.data : err.message; // sino agrega el error 
+                }
+            });
     });
 });
