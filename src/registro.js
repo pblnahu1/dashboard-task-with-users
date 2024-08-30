f993187d4c043b5e04d4b47e0577076ea387ae2d
Pablo Torrez <pablotorrez90@outlook.com>
2024-08-30 19:15:17 -0300

    arreglando bugs

--- src/registro.js
+++ src/registro.js
@@ -5,40 +5,66 @@
 
 // import axios from 'axios';
 
-const infoConnection = document.getElementById("connection-status-register");
-document.addEventListener('DOMContentLoaded', () => {
-    const form = document.getElementById("register-form");
-    // console.log(form === null);
-    form.addEventListener("submit", (event) => {
-        event.preventDefault();
-        const nombreApellido = document.getElementById("name-and-lastname-register").value;
-        // console.log(nombreApellido)
-        const email = document.getElementById("email-register").value;
-        // console.log(email)
-        const password = document.getElementById("password-register").value;
-        // console.log(password)
+const fnAddLoader = () => document.getElementById('loader').classList.add('hidden');
 
-        // hago una solicitud POST al servidor, con la URL del endpoint del servidor y los datos del usuario que se envían en la solicitud para registrar al usuario
-        axios.post('http://localhost:8081/register', { nombreApellido, email, password })
-            .then(res => {
-                console.log(res); // { data: {...}, status: 200, statusText: 'OK', headers: {...}, config: {...} }
-                console.log(res.data); // {message: '¡Registrado Exitosamente!'}
-                if (res.data.message === "¡Registrado Exitosamente!") {
-                    console.log(res); // { data: {...}, status: 200, statusText: 'OK', headers: {...}, config: {...} }
-                    console.log(res.data); // { message: '¡Registrado Exitosamente!' }
-                    console.log(res.data.message); // ¡Registrado Exitosamente!
-                    infoConnection.innerText = res.data.message; // Almacena en el elemento <span> el mensaje
-                }
-            })
-            .catch(err => {
-                console.log("Hubo un error al registrar el usuario: ", err.response ? err.response.data : err.message)
-                if(infoConnection.innerText.indexOf("Hubo un error al registrar el usuario") === -1){ // le pongo '-1' para preguntar si no hay 'Hubo un error al registrar el usuario'
-                    infoConnection.innerText = "Hubo un error al registrar el usuario"; // lo agrega si no hay
-                } else if (infoConnection.innerText.indexOf("Registro exitoso") === -1){ // sino, si el contenido no tiene "Registro exitoso" que lo agregue
-                    infoConnection.innerText = "Registro exitoso"; // lo agrega si no hay
-                } else {
-                    infoConnection.innerText = err.response ? err.response.data : err.message; // sino agrega el error 
-                }
-            });
-    });
+const fnRemoveLoader = () => document.getElementById('loader').classList.remove('hidden');
+
+const reiniceInput = () => {
+  document.getElementById("name-and-lastname-register").value = '';
+  document.getElementById("email-register").value = '';
+  document.getElementById("password-register").value = '';
+}
+
+const infoConnection = document.getElementById("connection-status-register");
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  const form = document.getElementById("register-form");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    fnRemoveLoader();
+
+    const nombreApellido = document.getElementById("name-and-lastname-register").value;
+    const email = document.getElementById("email-register").value;
+    const password = document.getElementById("password-register").value;
+
+    // hago una solicitud POST al servidor, con la URL del endpoint del servidor y los datos del usuario que se envían en la solicitud para registrar al usuario
+    axios.post('http://localhost:8081/register', { nombreApellido, email, password })
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+
+        if (res.data.success) {
+          console.log(res.data);
+
+          infoConnection.innerText = res.data.message;
+
+          const welcomeMessage = document.createElement("h2");
+          welcomeMessage.textContent = "¡Bienvenido " + nombreApellido + "!";
+          welcomeMessage.classList.add("text-2xl", "font-bold", "m-4");
+
+          const registerContainer = document.getElementById('container-auth-register');
+          registerContainer.innerHTML = "";
+          registerContainer.appendChild(welcomeMessage);
+
+          const authSectionElement = document.getElementById('sct-auth');
+          authSectionElement.style.display = 'none';
+          reiniceInput();
+
+          setTimeout(() => {
+            window.location.href = '../index.html';
+          }, 2000)
+        } else {
+          infoConnection.innerText = res.data.message || "Error al registrar el usuario";
+        }
+      })
+      .catch(err => {
+        console.error("Error al registrar el usuario. Su cuenta ya existe: ", err.response ? err.response.data : err.message);
+        infoConnection.innerText = err.response?.data?.message || "Error al registrar el usuario. Su cuenta ya existe";
+      })
+      .finally(() => {
+        fnAddLoader();
+      });
+  });
 });
