d5ddf32e4b637480b13c95394fc2a878258e7623
Pablo Torrez <pablotorrez90@outlook.com>
2024-10-07 08:31:33 -0300

    corrigiendo un error en modules

--- api/rutas.js
+++ api/rutas.js
@@ -1,10 +1,5 @@
 // variables de entorno
 // require('dotenv').config()
 // const PORT = process.env.PORT || 8081;
-const PORT = 8081;
-const API_URL = 'http://localhost:8081';
-
-export {
-    PORT,
-    API_URL
-}
+export const PORT = 8081;
+export const API_URL = 'http://localhost:8081';
